73bee5cc09b0003491fb2ea786660ae4ed0fea0a
Tiago Rogato <70462857+TRogato@users.noreply.github.com>
2025-08-12 08:22:41 -0300

    Correções

---
+++ TRogato.github.io/php/sender.php
@@ -0,0 +1,33 @@
+<?php
+
+// Destinatário
+$para = "t.rogato@gmail.com";
+
+// Assunto do e-mail
+$assunto = "Contato do através do site https://trogato.github.io/meu-site-pessoal/";
+
+// Campos do formulário de contato
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$telefone = $_POST['telephone'];
+$mensagem = $_POST['content'];
+
+// Monta o corpo da mensagem com os campos
+$corpo = "<html><body>";
+$corpo .= "Nome: $nome ";
+$corpo .= "Email: $email Telefone: $telefone Mensagem: $mensagem";
+$corpo .= "</body></html>";
+
+// Cabeçalho do e-mail
+$email_headers = implode("\n", array("From: $nome", "Reply-To: $email", "Subject: $assunto", "Return-Path: $email", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
+
+//Verifica se os campos estão preenchidos para enviar então o email
+if (!empty($nome) && !empty($email) && !empty($mensagem)) {
+    mail($para, $assunto, $corpo, $email_headers);
+    $msg = "SHAAAZAAAAM!!! Sua mensagem foi enviada com sucesso.";
+    echo "<script>alert('$msg');window.location.assign('https://trogato.github.io/meu-site-pessoal/contato.html');</script>";
+} else {
+    $msg = "HOUSTON, WE HAVE A PROBLEM... Erro ao enviar a mensagem.";
+    echo "<script>alert('$msg');window.location.assign('https://trogato.github.io/meu-site-pessoal/contato.html');</script>";
+}
+?>
